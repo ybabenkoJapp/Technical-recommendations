4d35aed6f98557237f0d7ca183340db9c13f2707
ybabenkoJapp <ybabenko@jappware.com>
2023-11-04 19:54:58 +0200

    chore(axios,vuetify): add deps and use them in vue

--- Practical/vue-task/vue-application/src/main.ts
+++ Practical/vue-task/vue-application/src/main.ts
@@ -2,12 +2,28 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(App)
 
+app.use(vuetify)
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)
 app.use(createPinia())
 app.use(router)
 

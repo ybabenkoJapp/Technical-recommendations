bddc18422ee5c981e8c2b7994e1f99afe5d5da97
ybabenkoJapp <ybabenko@jappware.com>
2023-11-06 22:47:08 +0200

    refactor(Post.vue, CreatePost.vue): rename component Post.vue to CreatePost.vue and add emit events

---
+++ Practical/vue-task/vue-application/src/components/CreatePost.vue
@@ -0,0 +1,114 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import axios from "axios";
+
+const route = useRoute();
+const id = route.params.id; // read parameter id (it is reactive) // see https://stackoverflow.com/questions/65284428/how-to-get-params-of-router-in-vue-3#answer-66953246
+
+const title = ref("");
+const postBody = ref("");
+const emit = defineEmits<{
+  (
+    e: "push-post",
+    value: {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+    },
+  ): void;
+}>();
+
+const submitForm = () => {
+  axios
+    .post("posts", {
+      title: title.value,
+      body: postBody.value,
+      userId: 1,
+      config: {
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      },
+    })
+    .then((json) => {
+      emit("push-post", json.data);
+    })
+    .catch((error) => console.error(error));
+};
+</script>
+
+<template>
+  <div class="post-container">
+    <p>Check v-model Title: {{ title }}</p>
+    <p>Check v-model Body: {{ postBody }}</p>
+    <form class="post-form" @submit.prevent="submitForm">
+      <h2 class="post-title">Create Post</h2>
+      <div class="input-group">
+        <label for="title">Title</label>
+        <input
+          id="title"
+          v-model="title"
+          class="input-control"
+          name="title"
+          placeholder="Title"
+          type="text"
+        />
+      </div>
+      <div class="input-group">
+        <label for="post-editor">Body</label>
+        <textarea
+          id="post-editor"
+          v-model="postBody"
+          class="input-control"
+          cols="5"
+          name="post-editor"
+          placeholder="Post body"
+          rows="6"
+          type="text"
+        />
+      </div>
+      <button class="btn" type="submit">Save</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.post-form {
+  margin: 30px auto;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.post-title {
+  margin-bottom: 20px;
+}
+
+.input-group {
+  margin-bottom: 16px;
+}
+
+.input-group label {
+  display: block;
+  margin-bottom: 8px;
+  user-select: none;
+}
+
+.input-control {
+  display: block;
+  width: 100%;
+  font-size: 1.1rem;
+  background: #fff;
+  border: 1px solid #2d2c2c;
+  line-height: 1.6rem;
+  padding: 0.75rem 1rem;
+  border-radius: 3px;
+  height: 52px;
+}
+
+textarea.input-control {
+  height: unset !important;
+}
+</style>

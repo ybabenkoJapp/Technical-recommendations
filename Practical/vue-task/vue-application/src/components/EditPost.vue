ac668d945988b00a8cf25eda58cb7f6b14f47d78
ybabenkoJapp <ybabenko@jappware.com>
2023-11-18 11:18:45 +0200

    refactor(EditPost.vue): commented script logic and add form component

---
+++ Practical/vue-task/vue-application/src/components/EditPost.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import Form from "@/components/Form.vue";
+
+// const title = ref("");
+// const postBody = ref("");
+// const emit = defineEmits<{
+//   (
+//     e: "put-post",
+//     value: {
+//       userId: number;
+//       id: number;
+//       title: string;
+//       body: string;
+//     },
+//   ): void;
+// }>();
+// const submitForm = () => {
+//   axios
+//     .post("put", {
+//       title: title.value,
+//       body: postBody.value,
+//       userId: 1,
+//       config: {
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8",
+//         },
+//       },
+//     })
+//     .then((json) => {
+//       emit("put-post", json.data);
+//     })
+//     .catch((error) => console.error(error));
+// };
+</script>
+
+<template>
+  <div class="post-container">
+    <h2 class="post-title">Edit Post</h2>
+    <Form />
+  </div>
+</template>
+
+<style scoped>
+.post-form {
+  margin: 30px auto;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.post-title {
+  margin-bottom: 20px;
+}
+
+.input-group {
+  margin-bottom: 16px;
+}
+
+.input-group label {
+  display: block;
+  margin-bottom: 8px;
+  user-select: none;
+}
+
+.input-control {
+  display: block;
+  width: 100%;
+  font-size: 1.1rem;
+  background: #fff;
+  border: 1px solid #2d2c2c;
+  line-height: 1.6rem;
+  padding: 0.75rem 1rem;
+  border-radius: 3px;
+  height: 52px;
+}
+
+textarea.input-control {
+  height: unset !important;
+}
+
+/* input validation */
+textarea:invalid:required,
+input:invalid:required {
+  border: 1px solid red;
+}
+
+/*input:invalid:required {*/
+/*  background-image: linear-gradient(to right, pink, lightgreen);*/
+/*}*/
+
+input:valid {
+  border: 2px solid black;
+}
+</style>

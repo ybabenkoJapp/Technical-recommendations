c65f4d5b82b1c7efeabf9a8c561b961e61dd960a
ybabenkoJapp <ybabenko@jappware.com>
2023-11-18 11:20:41 +0200

    refactor(Form.vue): override component with html form

--- Practical/vue-task/vue-application/src/components/Form.vue
+++ Practical/vue-task/vue-application/src/components/Form.vue
@@ -1,49 +1,73 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import axios from "axios";
 
-const valid = ref(false);
 const title = ref("");
-const body = ref("");
-const nameRules = [
-  (value) => {
-    if (value) return true;
+const postBody = ref("");
 
-    return "Name is required.";
-  },
-  (value) => {
-    if (value?.length <= 10) return true;
+const emit = defineEmits<{
+  (
+    e: "put-post",
+    value: {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+    },
+  ): void;
+}>();
 
-    return "Name must be less than 10 characters.";
-  },
-];
+const submitForm = () => {
+  axios
+    .post("put", {
+      title: title.value,
+      body: postBody.value,
+      userId: 1,
+      config: {
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      },
+    })
+    .then((json) => {
+      emit("put-post", json.data);
+    })
+    .catch((error) => console.error(error));
+};
 </script>
 
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-text-field
-            v-model="title"
-            :counter="10"
-            :rules="nameRules"
-            hide-details
-            label="Post title"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col>
-          <v-text-field
-            v-model="body"
-            :counter="10"
-            :rules="nameRules"
-            hide-details
-            label="Post body"
-            required
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+  <form class="post-form" @submit.prevent="submitForm">
+    <div class="input-group">
+      <label for="title">Title</label>
+      <input
+        id="title"
+        v-model="title"
+        class="input-control"
+        minlength="3"
+        name="title"
+        placeholder="Title"
+        required
+        type="text"
+      />
+      <span aria-live="polite" class="error"></span>
+    </div>
+    <div class="input-group">
+      <label for="post-editor">Body</label>
+      <textarea
+        id="post-editor"
+        v-model="postBody"
+        class="input-control"
+        cols="5"
+        minlength="3"
+        name="post-editor"
+        placeholder="Post body"
+        required
+        rows="6"
+        type="text"
+      />
+      <span aria-live="polite" class="error"></span>
+    </div>
+    <button class="btn" type="submit">Save</button>
+  </form>
 </template>

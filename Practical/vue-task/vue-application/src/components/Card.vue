62583d356b4ce24f58e2a03e8e2c16c4f1308bf4
ybabenkoJapp <ybabenko@jappware.com>
2023-11-18 11:16:47 +0200

    feat(Card.vue): create card component

---
+++ Practical/vue-task/vue-application/src/components/Card.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import TeleportModal from "@/modals/TeleportModal.vue";
+import EditPost from "@/components/EditPost.vue";
+import { ref } from "vue";
+
+const props = defineProps<{ title: string; body: string }>();
+
+const { title, body } = props;
+const showModal = ref(false);
+</script>
+
+<template>
+  <v-card class="mx-auto my-5 position-relative" width="1000">
+    <template v-slot:title> {{ title }}</template>
+    <button @click="showModal = !showModal">Edit post</button>
+    <TeleportModal :show="showModal">
+      <EditPost />
+    </TeleportModal>
+    <v-card-text>
+      <p>{{ body }}</p>
+      <br />
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped></style>

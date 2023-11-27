58b0c5dfbd8e93216daf0c91ce1d2d20b0d7d967
ybabenkoJapp <ybabenko@jappware.com>
2023-11-27 10:55:33 +0200

    style(Form.vue): add styles to form

--- Practical/vue-task/vue-application/src/components/Form.vue
+++ Practical/vue-task/vue-application/src/components/Form.vue
@@ -54,3 +54,56 @@ const submitForm = () => {
     <button class="btn" type="submit">Save</button>
   </form>
 </template>
+
+<style scoped>
+.post-form {
+  margin: 30px auto;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+
+.post-title {
+  margin-bottom: 20px;
+}
+
+.input-group {
+  margin-bottom: 16px;
+}
+
+.input-group label {
+  display: block;
+  margin-bottom: 8px;
+  user-select: none;
+}
+
+.input-control {
+  display: block;
+  width: 100%;
+  font-size: 1.1rem;
+  background: #fff;
+  border: 1px solid #2d2c2c;
+  line-height: 1.6rem;
+  padding: 0.75rem 1rem;
+  border-radius: 3px;
+  height: 52px;
+}
+
+textarea.input-control {
+  height: unset !important;
+}
+
+/* input validation */
+textarea:invalid:required,
+input:invalid:required {
+  border: 1px solid red;
+}
+
+/*input:invalid:required {*/
+/*  background-image: linear-gradient(to right, pink, lightgreen);*/
+/*}*/
+
+input:valid {
+  border: 2px solid black;
+}
+</style>

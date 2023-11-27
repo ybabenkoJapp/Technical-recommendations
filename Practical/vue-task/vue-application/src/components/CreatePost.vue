8aed53df996d6193461f73f0c93b1da5d2ab4292
ybabenkoJapp <ybabenko@jappware.com>
2023-11-27 10:47:04 +0200

    refactor(CreatePost.vue): move form to external component, remove event emit logic

--- Practical/vue-task/vue-application/src/components/CreatePost.vue
+++ Practical/vue-task/vue-application/src/components/CreatePost.vue
@@ -1,80 +1,15 @@
 <script lang="ts" setup>
 import { useRoute } from "vue-router";
-import { ref } from "vue";
-import axios from "axios";
+import Form from "@/components/Form.vue";
 
 const route = useRoute();
 const id = route.params.id; // read parameter id (it is reactive) // see https://stackoverflow.com/questions/65284428/how-to-get-params-of-router-in-vue-3#answer-66953246
-
-const title = ref("");
-const postBody = ref("");
-const emit = defineEmits<{
-  (
-    e: "push-post",
-    value: {
-      userId: number;
-      id: number;
-      title: string;
-      body: string;
-    },
-  ): void;
-}>();
-
-const submitForm = () => {
-  axios
-    .post("posts", {
-      title: title.value,
-      body: postBody.value,
-      userId: 1,
-      config: {
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      },
-    })
-    .then((json) => {
-      emit("push-post", json.data);
-    })
-    .catch((error) => console.error(error));
-};
 </script>
 
 <template>
   <div class="post-container">
     <h2 class="post-title">Create Post</h2>
-    <form class="post-form" @submit.prevent="submitForm">
-      <div class="input-group">
-        <label for="title">Title</label>
-        <input
-          id="title"
-          v-model="title"
-          class="input-control"
-          minlength="3"
-          name="title"
-          placeholder="Title"
-          required
-          type="text"
-        />
-        <span aria-live="polite" class="error"></span>
-      </div>
-      <div class="input-group">
-        <label for="post-editor">Body</label>
-        <textarea
-          id="post-editor"
-          v-model="postBody"
-          class="input-control"
-          cols="5"
-          minlength="3"
-          name="post-editor"
-          placeholder="Post body"
-          required
-          rows="6"
-          type="text"
-        />
-        <span aria-live="polite" class="error"></span>
-      </div>
-      <button class="btn" type="submit">Save</button>
-    </form>
+    <Form />
   </div>
 </template>
 

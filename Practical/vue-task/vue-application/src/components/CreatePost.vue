a76038b4b6674e10bf80a7f75cea30d718a46d16
ybabenkoJapp <ybabenko@jappware.com>
2023-11-29 21:46:45 +0200

    refactor(CreatePost.vue): remove route params

--- Practical/vue-task/vue-application/src/components/CreatePost.vue
+++ Practical/vue-task/vue-application/src/components/CreatePost.vue
@@ -1,9 +1,5 @@
 <script lang="ts" setup>
-import { useRoute } from "vue-router";
 import Form from "@/components/Form.vue";
-
-const route = useRoute();
-const id = route.params.id; // read parameter id (it is reactive) // see https://stackoverflow.com/questions/65284428/how-to-get-params-of-router-in-vue-3#answer-66953246
 </script>
 
 <template>

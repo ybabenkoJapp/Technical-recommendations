d32698b7f6afe95a0d57eb76d75916cb7c49c3e9
ybabenkoJapp <ybabenko@jappware.com>
2023-11-29 20:35:19 +0200

    refactor(main.ts): add icon font for vuetify

--- Practical/vue-task/vue-application/src/main.ts
+++ Practical/vue-task/vue-application/src/main.ts
@@ -11,12 +11,28 @@ import router from "./router";
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
   components,
   directives,
+  defaults: {
+    VBtn: {
+      color: "blue",
+      variant: "outlined",
+      rounded: false,
+    },
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 const app = createApp(App);

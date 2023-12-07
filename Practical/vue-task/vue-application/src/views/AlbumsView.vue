55afb01a059a0cddd7713a2338d02faf9c06a5f4
ybabenkoJapp <ybabenko@jappware.com>
2023-12-07 16:25:35 +0200

    refactor(AlbumsView.vue): add store and html for rendering

--- Practical/vue-task/vue-application/src/views/AlbumsView.vue
+++ Practical/vue-task/vue-application/src/views/AlbumsView.vue
@@ -1,10 +1,34 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { useAlbumStore } from "@/stores";
+import { storeToRefs } from "pinia";
+import { onMounted } from "vue";
+
+const albumsStore = useAlbumStore();
+
+const { albums } = storeToRefs(albumsStore);
+onMounted(async () => await albumsStore.fetchAlbums());
 </script>
 
 <template>
-
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="album in albums"
+        v-if="albums.length"
+        :key="album.id"
+        cols="12"
+        md="4"
+        sm="6"
+      >
+        <v-card>
+          <v-card-title>{{ album.title }}</v-card-title>
+          <v-card-text>
+            <p>User ID: {{ album.userId }}</p>
+            <p>ID: {{ album.id }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col v-else><p>No comments available</p></v-col>
+    </v-row>
+  </v-container>
 </template>
-
-<style scoped>
-
-</style>

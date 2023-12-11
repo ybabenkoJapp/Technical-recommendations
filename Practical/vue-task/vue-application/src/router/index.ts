4055f269b601e2f6cd2ccbfbc5c4b606696935f0
ybabenkoJapp <ybabenko@jappware.com>
2023-12-11 23:49:25 +0200

    feat( router.index ): add basic auth for routes

--- Practical/vue-task/vue-application/src/router/index.ts
+++ Practical/vue-task/vue-application/src/router/index.ts
@@ -4,6 +4,8 @@ import PostsView from "@/views/PostsView.vue";
 import AlbumsView from "@/views/AlbumsView.vue";
 import PhotosView from "@/views/PhotosView.vue";
 import PostView from "@/views/PostView.vue";
+import { useAuthStore } from "@/stores";
+import Login from "@/views/Login.vue";
 
 export const routes = [
   {
@@ -11,37 +13,67 @@ export const routes = [
     name: "home",
     component: HomeView,
   },
-  // {
-  //   path: "/dashboard",
-  //   name: "dashboard",
-  //   component: DashboardView,
-  // },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
   {
     path: "/posts",
     name: "posts",
     component: PostsView,
+    meta: { requiresAuth: true },
   },
   // TODO add dynamic post
   {
     path: "/posts/:id",
     name: "post",
     component: PostView,
+    meta: { requiresAuth: true },
   },
   {
     path: "/albums",
     name: "albums",
     component: AlbumsView,
+    meta: { requiresAuth: true },
   },
   {
     path: "/photos",
     name: "photos",
     component: PhotosView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/users",
+    name: "users",
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  // Check if the route requires authentication
+  if (to.meta.requiresAuth) {
+    // Check if the user is authenticated
+    if (!authStore.isAuthenticated) {
+      // Redirect to login page if not authenticated
+      next({ name: "login" });
+    } else if (to.meta.editorOnly && authStore.getCurrentUser !== "editor") {
+      // Redirect to home if user is not an editor but tries to access an editor-only route
+      next({ name: "home" });
+    } else {
+      // Continue to the requested route
+      next();
+    }
+  } else {
+    // Continue to the requested route
+    next();
+  }
 });
 
 export default router;

33c20aa1ff50224b795db15a950eea88d1caaf80
ybabenkoJapp <ybabenko@jappware.com>
2023-12-12 10:28:26 +0200

    refactor( DashboardView.vue ): try to fix dashboard styles

--- Practical/vue-task/vue-application/src/views/DashboardView.vue
+++ Practical/vue-task/vue-application/src/views/DashboardView.vue
@@ -1,19 +1,45 @@
 <script lang="ts" setup>
-import { routes } from "@/router/index";
+import { routes } from "@/router";
 
 const links = routes.map((link) => ({ title: link.name, route: link.path }));
 </script>
 <template>
-  <v-navigation-drawer>
-    <v-list-item subtitle="Vuetify" title="My Application"></v-list-item>
+  <v-navigation-drawer app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="subtitle-2">My Application</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
     <v-divider></v-divider>
     <v-list class="py-1" dense flat nav>
       <v-list-item
         v-for="item in links"
-        :title="item.title"
+        :key="item.route"
         :to="item.route"
         link
-      ></v-list-item>
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<style>
+.v-navigation-drawer {
+  width: 250px;
+}
+
+@media only screen and (max-width: 1264px) {
+  .v-navigation-drawer {
+    width: 200px;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .v-navigation-drawer {
+    width: 100%;
+  }
+}
+</style>

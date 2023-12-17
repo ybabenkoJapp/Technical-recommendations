8121a3771bc2c940664ce0ee18bdd6280dfb8bb6
ybabenkoJapp <ybabenko@jappware.com>
2023-12-17 23:17:57 +0200

    refactor( users.ts ): update store

--- Practical/vue-task/vue-application/src/stores/users.ts
+++ Practical/vue-task/vue-application/src/stores/users.ts
@@ -3,33 +3,65 @@ import type { IUser } from "@/app-types/IUser";
 import axios from "axios";
 
 export const useUserStore = defineStore("user", {
-  state() {
+  state(): {
+    users: IUser[];
+    user: IUser | null;
+    role: string;
+  } {
     return {
-      users: [] as IUser[],
-      user: {} as IUser,
+      users: [],
+      user: null,
+      role: "",
     };
   },
   getters: {
     getUsers: (state) => state.users,
     getUser: (state) => state.user,
+    getUserRole: (state) => state.role,
   },
   actions: {
     async fetchAllUsers() {
       try {
         const response = await axios("users");
         if (response.status === 200) {
-          console.log(response.data);
           this.setUsers(response.data);
         }
-      } catch (e) {}
+      } catch (e) {
+        console.error(e);
+      }
     },
 
-    setUser(user: IUser) {
+    setUser(user: IUser | null, role: string) {
       this.user = user;
+      this.role = role;
+
+      if (!sessionStorage.getItem("role")) {
+        sessionStorage.setItem("role", role);
+      } else {
+        sessionStorage.removeItem("role");
+        sessionStorage.setItem("role", role);
+      }
+
+      if (!sessionStorage.getItem("user")) {
+        sessionStorage.setItem("user", JSON.stringify(user));
+      } else {
+        sessionStorage.removeItem("user");
+        sessionStorage.setItem("user", JSON.stringify(user));
+      }
     },
 
     setUsers(users: IUser[]) {
-      this.users.push(...this.users, ...users);
+      if (users.length) {
+        this.users.push(...users);
+        if (!sessionStorage.getItem("users")) {
+          sessionStorage.setItem("users", JSON.stringify(this.users));
+        } else {
+          sessionStorage.removeItem("users");
+          sessionStorage.setItem("users", JSON.stringify(this.users));
+        }
+      } else {
+        this.users.length = 0;
+      }
     },
   },
 });

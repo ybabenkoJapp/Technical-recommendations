1de01872fd9dd97ce8fa65e2bc64cc619078e2e3
ybabenkoJapp <ybabenko@jappware.com>
2023-12-17 23:19:16 +0200

    refactor( UsersView.vue ): add watchers

--- Practical/vue-task/vue-application/src/views/UsersView.vue
+++ Practical/vue-task/vue-application/src/views/UsersView.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 import { useUserStore } from "@/stores";
-import { computed, onMounted } from "vue";
+import { onMounted, ref, watch } from "vue";
 import type { IUser } from "@/app-types/IUser";
 
 const usersStore = useUserStore();
 
-onMounted(() => usersStore.fetchAllUsers());
+onMounted(() => {
+  const sessionUsers = sessionStorage.getItem("users");
+  if (sessionUsers) {
+    allUsers.value = JSON.parse(sessionUsers);
+  } else {
+    usersStore.fetchAllUsers();
+    allUsers.value = usersStore.getUsers;
+  }
+});
 
-const allUsers = computed<IUser[]>(() => usersStore.getUsers);
+watch(
+  () => usersStore.getUsers,
+  (newUsers) => (allUsers.value = newUsers),
+);
+
+const allUsers = ref<IUser[]>();
 </script>
 
 <template>
